e53bd5720f580f9bf4fc97d0f008d83f3c7d6f42
SergioMadrid522 <sergiofabian.madrid@hotmail.com>
2026-05-31 12:32:53 -0600

    implement spring boot project initialization

---
+++ backend/us-accidents-bff/src/main/java/com/sergio/usaccidentsbff/UsAccidentsBffApplication.java
@@ -0,0 +1,13 @@
+package com.sergio.usaccidentsbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsAccidentsBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsAccidentsBffApplication.class, args);
+	}
+
+}
---
+++ backend/us-accidents-bff/src/test/java/com/sergio/usaccidentsbff/UsAccidentsBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.usaccidentsbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsAccidentsBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
